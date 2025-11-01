98db25b94c86a3ff45f507bb4b9bbc825c02e16a
Gourav-25 <gaman411@gmail.com>
2025-11-01 23:09:12 +0530

    Update SampleTest2.java

--- src/test/java/Test1/SampleTest2.java
+++ src/test/java/Test1/SampleTest2.java
@@ -3,6 +3,7 @@ package Test1;
 public class SampleTest2 {
 
 	public static void main(String[] args) {
-      System.out.println("SampleTest2");
+        System.out.println("SampleTest2");
+		System.out.println("SampleTest3");
 	}
 }
