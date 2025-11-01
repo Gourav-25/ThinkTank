b363d1c78880828f79f9e6efa81ef2227a9d0247
Gourav-25 <gaman411@gmail.com>
2025-11-01 23:54:45 +0530

    Update SampleTest.java

--- src/test/java/Test/SampleTest.java
+++ src/test/java/Test/SampleTest.java
@@ -5,6 +5,8 @@ public class SampleTest {
 	public static void main(String[] args) {
 	System.out.println("mainjava");
 	System.out.println("TestJava");
+		System.out.println("bnhbbm");
+						   
 	}
 
 }
